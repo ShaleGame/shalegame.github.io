3fef7047b7c570a18cef44dcfeb610352a59c3d0
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2026-04-30 21:52:50 -0700

    add package @yeskunall/astro-umami instead of head script

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import yeskunallumami from '@yeskunall/astro-umami';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shalegame.github.io',
-  integrations: [preact()],
+  integrations: [preact(), yeskunallumami({ id: '6dd6e1c2-6bb4-47f0-89c3-3d760aabc60c' })],
   server: {
     headers: {
       'Content-Security-Policy': 'script-src \'self\' https://cloud.umami.is;'
